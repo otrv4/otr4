365de93f8e2bbe92616cc29f99f390c33f4ac1e4
Sofia Celi <sceli@thoughtworks.com>
2016-12-12 16:37:15 -0500

    adds version for smp generation and refactors test

--- smp.go
+++ smp.go
@@ -4,8 +4,11 @@ import (
 	sha3 "golang.org/x/crypto/sha3"
 )
 
+const smpVersion = 2
+
 func generateSMPsecret(intrFingerprint, recvrFingerprint, ssid, secret []byte) []byte {
 	h := sha3.New512()
+	h.Write([]byte{smpVersion})
 	h.Write(intrFingerprint)
 	h.Write(recvrFingerprint)
 	h.Write(ssid)
