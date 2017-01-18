fffda1c269f96aae9ed4fdeb8bb50e9bb6802261
Natalie Eskinazi <n.l.eskinazi@gmail.com>
2017-01-18 18:37:19 -0500

    YD & NE | 3 | Update hashToScalar function to use new ed448 function, change signature of auth function, update tests

--- dre.go
+++ dre.go
@@ -5,13 +5,24 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func auth(aPub, bPub, bPubEcdh *publicKey, aSec *secretKey, message []byte) (sigma [6]ed448.BigNumber) {
+type bigNumber [56]byte
+
+//TODO
+func auth(aPub, bPub, bPubEcdh *publicKey, aSec *secretKey, message []byte) (sigma [6]bigNumber) {
+	// message := "hello"
+	// t1, c2, c3, r2, r3 []byte || [16]uint32 := rand(q)
+	// T1 := ed448.MulByBase(t1)
+	// T2 := ed448.Add(ed448.MulByBase(r2), ed448.Mul(bPub, c2))
+	// T3 := ed448.Add(ed448.MulByBase(r3), ed448.Mul(bPubEcdh, c3))
+	// values := concat(basePoint, primeOrder, aPub, bPub, bPubEcdh, T1, T2, T3, message)
+	// c := hashToScalar(values)
+	// c1 := ed448.ModQ(ed448.Sub(ed448.Sub(c, c2), c3))
+	// r1 := ed448.ModQ(ed448.Sub(t1, ed448.Mul(c1, a2)))
+	// sigma = []bigNumber{c1, r1, c2, r2, c3, r3}
 	return
 }
 
-func hashToScalar(in []byte) (scalar ed448.BigNumber) {
+func hashToScalar(in []byte) (scalar []byte) {
 	hash := sha3.Sum512(in)
-	hashSlice := hash[0:64]
-	scalar = ed448.DeserializeModQ(hashSlice)
-	return
+	return ed448.ModQ(hash[:])
 }
