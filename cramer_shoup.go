32e63231fdab5294c3233ae4a105bca69edda68a
Sofia Celi <sceli@thoughtworks.com>
2017-02-16 16:35:06 -0500

    implements cramer shoup encryption

--- cramer_shoup.go
+++ cramer_shoup.go
@@ -8,6 +8,7 @@ import (
 	"github.com/twstrike/ed448"
 )
 
+// XXX: use bytes?
 type cramerShoupPrivateKey struct {
 	x1, x2, y1, y2, z ed448.Scalar
 }
@@ -16,14 +17,16 @@ type cramerShoupPublicKey struct {
 	c, d, h ed448.Point
 }
 
-// XXX: separate this function?
-func deriveCramerShoupKeys(rand io.Reader) (priv cramerShoupPrivateKey, pub cramerShoupPublicKey) {
+func deriveCramerShoupKeys(rand io.Reader) (*cramerShoupPrivateKey, *cramerShoupPublicKey) {
 
-	priv.x1 = randomLongTermScalar(rand)
-	priv.x2 = randomLongTermScalar(rand)
-	priv.y1 = randomLongTermScalar(rand)
-	priv.y2 = randomLongTermScalar(rand)
-	priv.z = randomLongTermScalar(rand)
+	priv := &cramerShoupPrivateKey{}
+	pub := &cramerShoupPublicKey{}
+
+	priv.x1 = randLongTermScalar(rand)
+	priv.x2 = randLongTermScalar(rand)
+	priv.y1 = randLongTermScalar(rand)
+	priv.y2 = randLongTermScalar(rand)
+	priv.z = randLongTermScalar(rand)
 
 	pub.c = ed448.DoubleScalarMul(ed448.BasePoint, g2, priv.x1, priv.x2)
 	pub.d = ed448.DoubleScalarMul(ed448.BasePoint, g2, priv.y1, priv.y2)
@@ -32,14 +35,49 @@ func deriveCramerShoupKeys(rand io.Reader) (priv cramerShoupPrivateKey, pub cram
 	return priv, pub
 }
 
-func randomLongTermScalar(rand io.Reader) ed448.Scalar {
+// Part of cramer shoup suite
+// XXX: use a receiver
+func cramerShoupEnc(message []byte, rand io.Reader, pub *cramerShoupPublicKey) ([]byte, error) {
 
+	b := make([]byte, fieldBytes)
+	r, err := randScalar(rand, b)
+	if err != nil {
+		return nil, err
+	}
+
+	// u = G1*r, u2 = G2*r
+	u1 := ed448.PointScalarMul(ed448.BasePoint, r)
+	u2 := ed448.PointScalarMul(g2, r)
+
+	// e = (h*r) + m
+	m := ed448.NewPoint([16]uint32{}, [16]uint32{}, [16]uint32{}, [16]uint32{})
+	m.Decode(message, false)
+	e := ed448.PointScalarMul(pub.h, r)
+	e.Add(e, m)
+
+	// α = H(u1,u2,e)
+	a := concat(u1, u2, e)
+	hash := sha3.NewShake256()
+	hash.Write(a)
+	var alpha [fieldBytes]byte
+	hash.Read(alpha[:])
+
+	// v = c*r + d*(r * α)
+	tmp := ed448.NewDecafScalar(nil)
+	tmp.Mul(r, ed448.NewDecafScalar(alpha[:]))
+	v := ed448.DoubleScalarMul(pub.c, pub.d, r, tmp)
+
+	cipher := concat(u1, u2, e, v)
+	return cipher, nil
+}
+
+func randLongTermScalar(rand io.Reader) ed448.Scalar {
 	b := make([]byte, fieldBytes)
 	randScalar(rand, b)
 	hash := sha3.NewShake256()
 	hash.Write(b)
 	hash.Write([]byte("cramershoup_secret"))
-	var out [56]byte //is it ok? use 64 instead?
+	var out [fieldBytes]byte //is it ok? use 64 instead?
 	hash.Read(out[:])
-	return ed448.NewDecafScalar(out[:]) //check the decoding
+	return ed448.NewDecafScalar(out[:])
 }
