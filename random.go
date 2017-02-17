86ace661f53ea33d744aded6e04598ee12592f62
Sofia Celi <sceli@thoughtworks.com>
2017-02-17 15:59:06 -0500

    creates random file for random related functions

---
+++ random.go
@@ -0,0 +1,47 @@
+package otr4
+
+import (
+	"errors"
+	"io"
+
+	"golang.org/x/crypto/sha3"
+
+	"github.com/twstrike/ed448"
+)
+
+func randInto(r io.Reader, b []byte) error {
+	_, err := io.ReadFull(r, b)
+
+	if err != nil {
+		return errors.New(err.Error() + ": not enough bytes")
+	}
+	return nil
+}
+
+func randScalar(r io.Reader, b []byte) (ed448.Scalar, error) {
+	_, err := io.ReadFull(r, b)
+
+	if err != nil {
+		return nil, errors.New(err.Error() + ": not enough bytes")
+	}
+
+	return ed448.NewDecafScalar(b), nil
+}
+
+func randLongTermScalar(r io.Reader) ed448.Scalar {
+	b := make([]byte, fieldBytes)
+	err := randInto(r, b)
+
+	if err != nil {
+		return nil
+	}
+
+	hash := sha3.NewShake256()
+	hash.Write(b)
+	hash.Write([]byte("cramershoup_secret"))
+
+	var out [fieldBytes]byte
+	hash.Read(out[:])
+
+	return ed448.NewDecafScalar(out[:])
+}
