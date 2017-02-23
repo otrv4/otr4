3648adb229295962bdc0d7adbd95f6cfb4708406
Sofia Celi <sceli@thoughtworks.com>
2017-02-23 14:18:28 -0500

    moves code to appropiate place

--- dre.go
+++ dre.go
@@ -7,39 +7,6 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func auth(rand io.Reader, ourPub, theirPub, theirPubEcdh ed448.Point, ourSec ed448.Scalar, message []byte) ([]byte, error) {
-	ap, err := generateAuthParams(rand, 5)
-	if err != nil {
-		return nil, err
-	}
-	t1, c2, c3, r2, r3 := ap[0], ap[1], ap[2], ap[3], ap[4]
-	pt1 := ed448.PointScalarMul(ed448.BasePoint, t1)
-	pt2 := ed448.DoubleScalarMul(ed448.BasePoint, theirPub, r2, c2)
-	pt3 := ed448.DoubleScalarMul(ed448.BasePoint, theirPubEcdh, r3, c3)
-	c := concatAndHash(ed448.BasePoint, ed448.ScalarQ, ourPub,
-		theirPub, theirPubEcdh, pt1, pt2, pt3, message)
-	c1, r1 := ed448.NewDecafScalar(nil), ed448.NewDecafScalar(nil)
-	c1.Sub(c, c2)
-	c1.Sub(c1, c3)
-	r1.Mul(c1, ourSec)
-	r1.Sub(t1, r1)
-	sigma := concat(c1, r1, c2, r2, c3, r3)
-	return sigma, err
-}
-
-func verify(theirPub, ourPub, ourPubEcdh ed448.Point, sigma, message []byte) bool {
-	ps := parse(sigma)
-	c1, r1, c2, r2, c3, r3 := ps[0], ps[1], ps[2], ps[3], ps[4], ps[5]
-	pt1 := ed448.DoubleScalarMul(ed448.BasePoint, theirPub, r1, c1)
-	pt2 := ed448.DoubleScalarMul(ed448.BasePoint, ourPub, r2, c2)
-	pt3 := ed448.DoubleScalarMul(ed448.BasePoint, ourPubEcdh, r3, c3)
-	c := concatAndHash(ed448.BasePoint, ed448.ScalarQ, theirPub, ourPub, ourPubEcdh, pt1, pt2, pt3, message)
-	out := ed448.NewDecafScalar(nil)
-	out.Add(c1, c2)
-	out.Add(out, c3)
-	return c.Equals(out)
-}
-
 func drEnc(message []byte, rand io.Reader, pub1, pub2 *cramerShoupPublicKey) (ed448.Point, ed448.Point, error) {
 
 	bytes1 := make([]byte, fieldBytes)
@@ -100,6 +67,53 @@ func drEnc(message []byte, rand io.Reader, pub1, pub2 *cramerShoupPublicKey) (ed
 	return v1, v2, nil
 }
 
+func auth(rand io.Reader, ourPub, theirPub, theirPubEcdh ed448.Point, ourSec ed448.Scalar, message []byte) ([]byte, error) {
+	ap, err := generateAuthParams(rand, 5)
+	if err != nil {
+		return nil, err
+	}
+	t1, c2, c3, r2, r3 := ap[0], ap[1], ap[2], ap[3], ap[4]
+	pt1 := ed448.PointScalarMul(ed448.BasePoint, t1)
+	pt2 := ed448.DoubleScalarMul(ed448.BasePoint, theirPub, r2, c2)
+	pt3 := ed448.DoubleScalarMul(ed448.BasePoint, theirPubEcdh, r3, c3)
+	c := concatAndHash(ed448.BasePoint, ed448.ScalarQ, ourPub,
+		theirPub, theirPubEcdh, pt1, pt2, pt3, message)
+	c1, r1 := ed448.NewDecafScalar(nil), ed448.NewDecafScalar(nil)
+	c1.Sub(c, c2)
+	c1.Sub(c1, c3)
+	r1.Mul(c1, ourSec)
+	r1.Sub(t1, r1)
+	sigma := concat(c1, r1, c2, r2, c3, r3)
+	return sigma, err
+}
+
+func verify(theirPub, ourPub, ourPubEcdh ed448.Point, sigma, message []byte) bool {
+	ps := parse(sigma)
+	c1, r1, c2, r2, c3, r3 := ps[0], ps[1], ps[2], ps[3], ps[4], ps[5]
+	pt1 := ed448.DoubleScalarMul(ed448.BasePoint, theirPub, r1, c1)
+	pt2 := ed448.DoubleScalarMul(ed448.BasePoint, ourPub, r2, c2)
+	pt3 := ed448.DoubleScalarMul(ed448.BasePoint, ourPubEcdh, r3, c3)
+	c := concatAndHash(ed448.BasePoint, ed448.ScalarQ, theirPub, ourPub, ourPubEcdh, pt1, pt2, pt3, message)
+	out := ed448.NewDecafScalar(nil)
+	out.Add(c1, c2)
+	out.Add(out, c3)
+	return c.Equals(out)
+}
+
+func generateAuthParams(rand io.Reader, n int) ([]ed448.Scalar, error) {
+	bytes := make([]byte, fieldBytes*n)
+	var out []ed448.Scalar
+
+	for i := 0; i < n; i++ {
+		scalar, err := randScalar(rand, bytes[i*fieldBytes:(i+1)*fieldBytes])
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, scalar)
+	}
+	return out, nil
+}
+
 func parse(bytes []byte) []ed448.Scalar {
 	var out []ed448.Scalar
 
@@ -117,18 +131,6 @@ func parsePoint(bytes []byte) []ed448.Point {
 		out = append(out, ed448.NewPointFromBytes(bytes[i:i+fieldBytes]))
 	}
 	return out
-}
-
-func concatAndHash(bytes ...interface{}) ed448.Scalar {
-	return hashToScalar(concat(bytes...))
-}
-
-func hashToScalar(in []byte) ed448.Scalar {
-	hash := make([]byte, fieldBytes)
-	sha3.ShakeSum256(hash, in)
-	s := ed448.NewDecafScalar(nil)
-	s.Decode(hash)
-	return s
 }
 
 func concat(bytes ...interface{}) (b []byte) {
@@ -150,16 +152,6 @@ func concat(bytes ...interface{}) (b []byte) {
 	return b
 }
 
-func generateAuthParams(rand io.Reader, n int) ([]ed448.Scalar, error) {
-	bytes := make([]byte, fieldBytes*n)
-	var out []ed448.Scalar
-
-	for i := 0; i < n; i++ {
-		scalar, err := randScalar(rand, bytes[i*fieldBytes:(i+1)*fieldBytes])
-		if err != nil {
-			return nil, err
-		}
-		out = append(out, scalar)
-	}
-	return out, nil
+func concatAndHash(bytes ...interface{}) ed448.Scalar {
+	return hashToScalar(concat(bytes...))
 }
