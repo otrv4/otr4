a347aad85450c87fa8f90810c11260d9742ef172
Sofia Celi <sceli@thoughtworks.com>
2017-03-15 18:34:27 -0500

    adds new error regarding profile version

--- errors.go
+++ errors.go
@@ -3,6 +3,7 @@ package otr4
 var errInvalidPublicKey = newOtrError("not a valid public key")
 var notEnoughEntropy = newOtrError("cannot source enough entropy")
 var errImpossibleToDecrypt = newOtrError("cannot decrypt the message")
+var errInvalidVersion = newOtrError("no valid version agreement could be found")
 
 type otrError struct {
 	msg string
