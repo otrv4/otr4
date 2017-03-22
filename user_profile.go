f31bc42393f7dc6e677fa299d1624f7252275e3b
Sofia Celi <sceli@thoughtworks.com>
2017-03-22 12:06:16 -0500

    implements serialization of user profile params as well as user profile itself

--- user_profile.go
+++ user_profile.go
@@ -1,20 +1,24 @@
 package otr4
 
 import (
-	"math/big"
+	"bytes"
+	"encoding/binary"
 	"strings"
 )
 
 type signature [112]byte
 
+type dsaSignature [40]byte
+
 type userProfile struct {
 	versions        string
 	pubKey          *cramerShoupPublicKey
 	expiration      uint64
-	sig             signature
-	transitionalSig *big.Int // XXX: for the moment, use MPI?
+	sig             *signature
+	transitionalSig *dsaSignature
 }
 
+// XXX: include the other params
 func newProfile(v string) (*userProfile, error) {
 	if len(v) == 0 {
 		return nil, errInvalidVersion
@@ -27,4 +31,44 @@ func newProfile(v string) (*userProfile, error) {
 	profile := &userProfile{versions: v}
 
 	return profile, nil
+}
+
+// XXX: make this append?
+func serializeSig(sig *signature) []byte {
+	var binBuf bytes.Buffer
+
+	binary.Write(&binBuf, binary.BigEndian, sig)
+
+	return binBuf.Bytes()
+}
+
+func serializeTransitionalSig(sig *dsaSignature) []byte {
+	var binBuf bytes.Buffer
+
+	binary.Write(&binBuf, binary.BigEndian, sig)
+
+	return binBuf.Bytes()
+}
+
+func serializeBody(profile *userProfile) []byte {
+	var out []byte
+
+	out = appendData(out, parseToByte(profile.versions))
+	out = append(out, profile.pubKey.serialize()...)
+	out = appendWord64(out, profile.expiration)
+
+	return out
+}
+
+//XXX: this will be the signed verson.
+func (profile *userProfile) serialize() []byte {
+	var out []byte
+
+	out = appendData(out, parseToByte(profile.versions))
+	out = append(out, profile.pubKey.serialize()...)
+	out = appendWord64(out, profile.expiration)
+	out = append(out, serializeSig(profile.sig)...)
+	out = append(out, serializeTransitionalSig(profile.transitionalSig)...)
+
+	return out
 }
