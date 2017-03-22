06663308abd50039facc7f1a83a4c599acadf0d6
Sofia Celi <sceli@thoughtworks.com>
2017-03-22 18:12:11 -0500

    refactors random functions

--- random.go
+++ random.go
@@ -8,25 +8,41 @@ import (
 	"github.com/twstrike/ed448"
 )
 
-func randScalar(r io.Reader) (ed448.Scalar, error) {
-	b := make([]byte, fieldBytes)
-	_, err := io.ReadFull(r, b)
+func randSymKey(rand io.Reader) ([]byte, error) {
+	var b [symKeyBytes]byte
+
+	_, err := io.ReadFull(rand, b[:])
 	if err != nil {
 		return nil, notEnoughEntropy
 	}
-	return ed448.NewScalar(b), nil
+
+	return b[:], nil
 }
 
-func randLongTermScalar(r io.Reader) (ed448.Scalar, error) {
-	b := make([]byte, fieldBytes)
-	_, err := io.ReadFull(r, b)
+func randScalar(rand io.Reader) (ed448.Scalar, error) {
+	var b [fieldBytes]byte
+
+	_, err := io.ReadFull(rand, b[:])
 	if err != nil {
 		return nil, notEnoughEntropy
 	}
-	hash := sha3.NewShake256()
-	hash.Write(b)
-	hash.Write([]byte("cramershoup_secret"))
+
+	return ed448.NewScalar(b[:]), nil
+}
+
+func randLongTermScalar(rand io.Reader) (ed448.Scalar, error) {
+	var b [fieldBytes]byte
 	var out [fieldBytes]byte
+
+	_, err := io.ReadFull(rand, b[:])
+	if err != nil {
+		return nil, notEnoughEntropy
+	}
+
+	hash := sha3.NewShake256()
+	hash.Write(b[:])
+	hash.Write([]byte("cramershoup_secret"))
 	hash.Read(out[:])
+
 	return ed448.NewScalar(out[:]), nil
 }
