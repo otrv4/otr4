47a54e53bbb163b9be279cb06eec41a9f90d8f5f
Sofia Celi <sceli@thoughtworks.com>
2017-03-27 17:29:50 -0500

    implements basic smp functions

--- smp.go
+++ smp.go
@@ -1,6 +1,7 @@
 package otr4
 
 import (
+	"github.com/twstrike/ed448"
 	"golang.org/x/crypto/sha3"
 )
 
@@ -14,4 +15,18 @@ func generateSMPsecret(initiatorFingerprint, receiverFingerprint, ssid, secret [
 	h.Write(ssid)
 	h.Write(secret)
 	return h.Sum(nil)
+}
+
+func generateDZKP(r, a, c ed448.Scalar) ed448.Scalar {
+	a.Mul(a, c)
+	r.Sub(r, a)
+	return r
+}
+
+func generateZKP(r, a ed448.Scalar, ix byte) (ed448.Scalar, ed448.Scalar) {
+	gr := ed448.PrecomputedScalarMul(r)
+	c := hashToScalar(ix, gr)
+	d := generateDZKP(r, a, c)
+
+	return c, d
 }
