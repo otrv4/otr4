4decc2807868f468145e9380e333930e31794185
Sofia Celi <sceli@thoughtworks.com>
2017-04-06 12:49:03 -0500

    implements ZKPK for SMP

--- smp.go
+++ smp.go
@@ -31,11 +31,38 @@ func generateZKP(r, a ed448.Scalar, ix byte) (ed448.Scalar, ed448.Scalar) {
 	return c, d
 }
 
-func verifyZKP(d, c ed448.Scalar, gen ed448.Point, ix byte) bool {
+func verifyZKP(d, c ed448.Scalar, g ed448.Point, ix byte) bool {
 	r := ed448.PrecomputedScalarMul(d)
-	s := ed448.PointScalarMul(gen, c)
+	s := ed448.PointScalarMul(g, c)
 	p := ed448.NewPointFromBytes()
 	p.Add(r, s)
 	t := hashToScalar(ix, p)
 	return c.Equals(t)
+}
+
+func verifyZKP2(g2, g3, pb, qb ed448.Point, d5, d6, cp ed448.Scalar, ix byte) bool {
+	l := ed448.PointDoubleScalarMul(g3, pb, d5, cp)
+	r := ed448.PointDoubleScalarMul(ed448.BasePoint, g2, d5, d6)
+	s := ed448.PointScalarMul(qb, cp)
+	r.Add(r, s)
+	t := hashToScalar(ix, l, r)
+	return cp.Equals(t)
+}
+
+func verifyZKP3(g2, g3, pa, qa ed448.Point, d5, d6, cp ed448.Scalar, ix byte) bool {
+	l := ed448.PointDoubleScalarMul(g3, pa, d5, cp)
+	r := ed448.PointDoubleScalarMul(ed448.BasePoint, g2, d5, d6)
+	s := ed448.PointScalarMul(qa, cp)
+	r.Add(r, s)
+	t := hashToScalar(ix, l, r)
+	return cp.Equals(t)
+}
+
+func verifyZKP4(g3a, qa, qb, ra ed448.Point, d7, cr ed448.Scalar, ix byte) bool {
+	s := ed448.NewPointFromBytes()
+	s.Sub(qa, qb)
+	l := ed448.PointDoubleScalarMul(ed448.BasePoint, g3a, d7, cr)
+	r := ed448.PointDoubleScalarMul(s, ra, d7, cr)
+	t := hashToScalar(ix, l, r)
+	return cr.Equals(t)
 }
